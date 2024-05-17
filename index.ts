2e8814233ed8bab3adde36ad418bd3a41f1297c1
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-05-17 13:26:00 +0100

    add comments & comment out consolelogs for future debugging

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ const ejs = require("ejs");
 require('dotenv').config();
 require("process");
 
+// Declare array to used for games on sale using interface //
 
 const gamesOnSaleArr: boardGameData[] = []
 
@@ -16,8 +17,7 @@ async function getBoardGamePrices() {
     const context = await browser.newContext();
     const page = await context.newPage();
 
-    // Navigate to product page 
-
+    // Loop through each game in boardGameProducts array //
     for (const boardGame of boardGameProducts) {
 
         let priceString: string;
@@ -26,7 +26,6 @@ async function getBoardGamePrices() {
         let listPrice: number;
 
         // Connect to each product page //
-
         try {
             await page.goto(boardGame.gameUrl, { timeout: 2 * 60 * 1000 });
         } catch (error) {
@@ -35,38 +34,36 @@ async function getBoardGamePrices() {
                 throw new Error("Failed to establish connection. Aborting...")
             }
         }
-        console.log(`Connected to ${boardGame.gameTitle} Amazon product page!`)
+        // console.log(`Connected to ${boardGame.gameTitle} Amazon product page!`)
 
         // Get current price //
         try {
             const priceElement = page.locator('span.priceToPay').nth(0)
             priceString = await priceElement.textContent({ timeout: 3000 })
             price = parseFloat(priceString.replace(/£/, ''))
-            console.log(`Current Price = £${price}`)
+            // console.log(`Current Price = £${price}`)
         } catch (error) {
             console.error
             throw new Error("Could not find sale price. priceToPay may have been renamed.")
         }
-        // Get list price //
 
+        // Get list price //
         try {
             listPriceString = await page.locator('span.basisPrice .a-offscreen').nth(0).textContent({ timeout: 3000 })
             listPrice = parseFloat(listPriceString.replace(/£/, ''))
-            console.log(`List Price = £${listPrice}`)
+            // console.log(`List Price = £${listPrice}`)
         } catch (error) {
             listPrice = price
-            console.log(`List Price = £${listPrice}`)
-            console.error(`${boardGame.gameTitle} is NOT on sale.`)
+            // console.log(`List Price = £${listPrice}`)
+            // console.error(`${boardGame.gameTitle} is NOT on sale.`)
         }
 
         // Calculate % off //
-
         const difference = listPrice - price
         const percentageDifference = Math.ceil((difference / listPrice) * 100)
-        console.log(`${boardGame.gameTitle} is %${percentageDifference} off.`)
+        // console.log(`${boardGame.gameTitle} is %${percentageDifference} off.`)
 
         // Define new individualBoardGame object
-
         const individualBoardGame: boardGameData = {
             gameTitle: boardGame.gameTitle,
             gameUrl: boardGame.gameUrl,
@@ -76,29 +73,26 @@ async function getBoardGamePrices() {
         }
 
         // Determine whether or not game is on sale by checking if current price is less than list price. Push object to gamesonSaleArr if true. //
-
         if (price < listPrice) {
-            console.log(` ${boardGame.gameTitle} IS on sale.`)
+            // console.log(` ${boardGame.gameTitle} IS on sale.`)
             gamesOnSaleArr.push(individualBoardGame)
         }
     }
 
-    console.log(gamesOnSaleArr)
+    // console.log(gamesOnSaleArr)
 
+    // Close playwright chromium browser //
     await browser.close();
 
+    // Send email if there are any games on sale //
     if (gamesOnSaleArr.length > 0) {
         try {
             sendEmail()
         } catch (error) {
             console.error("Unable to send Email")
             console.error(error)
-
         }
-
     }
-
-
 }
 
 async function sendEmail() {
@@ -115,9 +109,7 @@ async function sendEmail() {
 
     const renderedHtml = await fs.promises.readFile('emailTemplate.ejs', 'utf8');
     const template = ejs.compile(renderedHtml); // Compile EJS template //
-
     const html = template({ gamesOnSaleArr }) // Pass desired data to template to be rendered in email //
-
 
     // function to send email //
     const message = await transporter.sendMail({
@@ -126,11 +118,7 @@ async function sendEmail() {
         subject: 'Board Game Offers',
         html: html,
     });
-    console.log("Email sent! Check your indox. " + message.messageId)
+    // console.log("Email sent! Check your indox. " + message.messageId)
 }
 
-
-
 getBoardGamePrices()
-
-// <(^.^)> //
